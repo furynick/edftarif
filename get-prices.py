19752f6655a3564474dff9432f257740219124c7
Nicolas T. <nicolas@furyweb.fr>
2024-12-10 11:13:04 +0100

    Source EDF
    Importation de la grille tarifaire EDF depuis le site Web

--- get-prices.py
+++ get-prices.py
@@ -1,13 +1,21 @@
+import urllib3
 from pypdf import PdfReader
-import re
+from io import BytesIO
+from re import search
 
-reader = PdfReader("https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/Grille_prix_Tarif_Bleu.pdf")
+url = "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/Grille_prix_Tarif_Bleu.pdf"
+http = urllib3.PoolManager()
+remoteFile = http.request("GET", url)
+memoryFile = BytesIO(remoteFile.data)
+reader = PdfReader(memoryFile)
+
 page = reader.pages[0]
 text = page.extract_text()
 lines = text.split("\n")
-found = False
+option = "None"
 for line in lines:
-  if "tempo" in line.lower():
-    found = True
-  if found and re.search("^[0-9]", line):
-    print(line)
+  if "option" in line.lower():
+    words = line.split(" ")
+    option = words[1].lower()
+  if option != "None" and search("^[0-9]", line):
+    print(option + " " + line)
